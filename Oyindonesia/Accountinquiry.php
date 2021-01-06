82553eb5b5b9ead0ce2ecb1517d6eedb85b5a161
reactmore <64960434+reactmore@users.noreply.github.com>
2021-01-06 20:20:24 +0700

    + Add New Method

--- Oyindonesia/Accountinquiry.php
+++ Oyindonesia/Accountinquiry.php
@@ -6,28 +6,58 @@ namespace Oyindonesia;
 
 class Accountinquiry
 {
-    //Get Account Balance
+  //Get Account Balance
 
-    public static function balance()
-    {
+  public static function balance() {
 
-        return ApiRequestor::get(
-            Config::getBaseUrl() . 'balance',
-            Config::$apikey,
-            Config::$username,
-            false
-        );
-    }
+    return ApiRequestor::get(
+      Config::getBaseUrl() . 'balance',
+      Config::$apikey,
+      Config::$username,
+      false
+    );
+  }
 
-    public static function getAccountinquiry($params)
-    {
-        $result = ApiRequestor::post(
-            Config::getBaseUrl() . 'account-inquiry',
-            Config::$apikey,
-            Config::$username,
-            $params
-        );
+  // account-inquiry Api
 
-        return $result;
-    }
+  public static function getAccountinquiry($params) {
+    $result = ApiRequestor::post(
+      Config::getBaseUrl() . 'account-inquiry',
+      Config::$apikey,
+      Config::$username,
+      $params
+    );
+
+    return $result;
+  }
+
+  /** Get Account Inquiry Invoices
+  *
+  *
+  * @method Get
+  */
+  public static function Invoices() {
+
+    return ApiRequestor::get(
+      Config::getBaseUrl() . 'account-inquiry/invoices',
+      Config::$apikey,
+      Config::$username,
+      false
+    );
+  }
+
+  /** Get Account Inquiry Invoices By Id
+  *
+  *
+  * @method Get
+  */
+  public static function InvoicesId($params) {
+
+    return ApiRequestor::get(
+      Config::getBaseUrl() . 'account-inquiry/invoices/' . $params,
+      Config::$apikey,
+      Config::$username,
+      false
+    );
+  }
 }
